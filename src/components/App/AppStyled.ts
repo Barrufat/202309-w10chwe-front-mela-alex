86307dc4c687347d07552bc607d8bb6832d206aa
Alex Barbero <barrufat@hotmail.com>
2023-11-18 14:42:45 +0100

    Add AppStyled

---
+++ src/components/App/AppStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AppStyled = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default AppStyled;
