185566a47974fa93084b94216972b2987f012dfb
Alex Barbero <barrufat@hotmail.com>
2023-11-18 17:39:08 +0100

    Add robotsPage and pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Navigate, Routes } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 import AppStyled from "./AppStyled";
+import RobotsPage from "../../pages/robotsPage/robotsPage";
 
 const App = () => {
   return (
     <AppStyled>
+      <Navigation />
       <h1>Robots App</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<RobotsPage />} />
+      </Routes>
     </AppStyled>
   );
 };
