22b80bf5e117b26cfb86f2b5e36a2f9d45621744
Alex Barbero <barrufat@hotmail.com>
2023-11-18 01:28:27 +0100

    Add mainTheme styles

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    yellow: "#060",
+  },
+  typography: {
+    mainFontFamily: "Lucida Sans",
+  },
+};
+
+export default mainTheme;
